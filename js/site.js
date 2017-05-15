dc99c8485f3114134c36c121970d5725f0a8b0e2
dtfiedler <dtfiedler@wisc.edu>
2017-05-15 10:46:14 -0500

    added contact us page and updated search bar validation

--- js/site.js
+++ js/site.js
@@ -69,6 +69,7 @@ var getGeoCoding = function () {
 				.error(function(e){
 					console.log(e);
 				});
+	 getWeather();
 }
 
 var search = function () {
@@ -76,10 +77,10 @@ var search = function () {
 		getGeoCoding();
 		$("#zip_input").removeClass("error");
 		$("#error").remove();
-	}
-	else{
+	} else {
 		console.log('error');
 		$("#zip_input").addClass("error");
+		$("#error").remove();
 		$("#btn").before("<div id='error'>Please enter a valid 5 digit zip code</div>");
 	}
 }
@@ -94,7 +95,7 @@ var isValid = function () {
 			return false
 		}
 
-}else {
+} else {
 	return false;
 }
 
@@ -102,23 +103,54 @@ var isValid = function () {
 
 var getWeather = function () {
     var zip = document.getElementById('zip_input').value;
-    console.log("zip is --> " + zip);
+    console.log("getting weather information");
     $.ajax({
            type: 'GET',
-                                                                                crossOrigin: true,
+           crossOrigin: true,
            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
            url: "https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + zip + "')&format=json&callback=callbackFunction"
         }).done(function(data){
-                        // Center the map at the requested zip code.
-                        document.getElementById('weather_div').innerText = data.responseText;
+						console.log("successfully retrieved weather");
+	            // Center the map at the requested zip code.
+	           document.getElementById('weather_div').innerText = data.responseText;
         })
         .error(function(e){
-                        // For some reason AJAX considers the return an error, although in some cases we receive a valid JSON response.
-                        var newString = JSON.parse(e.responseText.split("callbackFunction(")[1].split(")")[0]);
-                        if (newString.query.results) {
-                                        document.getElementById('weather_div').innerText = "Date: " +  newString.query.results.channel.item.condition.date + " Weather: " + newString.query.results.channel.item.condition.text + " Temp: " + newString.query.results.channel.item.condition.temp;
-                        } else {
-                                        document.getElementById('weather_div').innerText = "Null Result: Yahoo API Returning no weather currently";
-                        }
+						console.log(e);
+						// For some reason AJAX considers the return an error, although in some cases we receive a valid JSON response.
+						var newString = JSON.parse(e.responseText.split("callbackFunction(")[1].split(")")[0]);
+						if (newString.query.results) {
+							console.log("successfully retrieved weather");
+
+						                document.getElementById('weather_div').innerText = "Date: " +  newString.query.results.channel.item.condition.date + " Weather: " + newString.query.results.channel.item.condition.text + " Temp: " + newString.query.results.channel.item.condition.temp;
+						} else {
+						                document.getElementById('weather_div').innerText = "Null Result: Yahoo API Returning no weather currently";
+						}
         });
+}
+
+var validateContactForm = function (){
+
+		var email = $('#email').val();
+		console.log(email);
+
+		var re = /^(([^<>()[\]{}'^?\\.,!|//#%*-+=&;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+		if (email.length > 3 && re.test(email)){
+				console.log("valid email");
+				$("#contactError").hide();
+				$('#email').removeClass('error');
+
+				var comment = $('#Comments').val();
+				var name = $('#name').val();
+
+				console.log(comment);
+				if (comment.length >= 3 && name.length >= 3){
+					console.log('success');
+					$('#contactUsForm').hide();
+					$('#message').show();
+				}
+		} else {
+			console.log("invalid email");
+			$("#contactError").show();
+			$('#email').addClass('error');
+		}
 }
